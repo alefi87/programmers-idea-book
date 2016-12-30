f745adc925ee7669327613d195a2f92cbff0257e
Alexander Filatov <alefi87@gmail.com>
2016-12-30 14:12:10 +0100

    Refactored Proper Decimal to Binary converter according to AirBnB ESLint style

--- numbers/decimal-binary-converter-proper/index.js
+++ numbers/decimal-binary-converter-proper/index.js
@@ -1,19 +1,20 @@
 'use strict';
 
-exports.dec2bin = (decimal)=>{
-	if (decimal !== parseInt(decimal))
-		return undefined;
-
-	// Coerse into unsigned int by logical shifting
-	// http://stackoverflow.com/questions/9939760/how-do-i-convert-an-integer-to-binary-in-javascript
-	return (decimal >>> 0).toString(2);
+exports.dec2bin = (decimal) => {
+  if (decimal !== parseInt(decimal, 10)) {
+    return undefined;
+  }
+  // Coerse into unsigned int by logical shifting
+  // http://stackoverflow.com/questions/9939760/how-do-i-convert-an-integer-to-binary-in-javascript
+  /* eslint no-bitwise: ["error", { "allow": [">>>"] }] */
+  return (decimal >>> 0).toString(2);
 };
 
-exports.bin2dec = (binary)=>{
-	if (typeof binary !== 'string' || binary.match(/[^10]/))
-		return undefined;
-
-	// This will never produce unsigned integer results
-	// I don't want to get into bit shifting magic here
-	return parseInt(binary, 2);
+exports.bin2dec = (binary) => {
+  if (typeof binary !== 'string' || binary.match(/[^10]/)) {
+    return undefined;
+  }
+  // This will never produce unsigned integer results
+  // I don't want to get into bit shifting magic here
+  return parseInt(binary, 2);
 };
