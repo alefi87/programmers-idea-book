6c871766926efd82b041bca0c929cff6d487b057
Alexander Filatov <alefi87@gmail.com>
2016-12-30 13:43:08 +0100

    Refactored Change calculator according to AirBnB ESLint rules. Added promises.

--- numbers/change-return/index.js
+++ numbers/change-return/index.js
@@ -1,51 +1,51 @@
 'use strict';
 
-var prompt = require('prompt');
-var change = require('./change');
-var _ = require('lodash');
-var prices = {
-	'Chocolate Bar': 1.23,
-	'Candy': 0.07,
-	'Drink': 1.99
+const prompt = require('prompt');
+const Promise = require('bluebird');
+const calculateChange = require('./change').calculate;
+const _ = require('lodash');
+
+const prices = {
+  'Chocolate Bar': 1.23,
+  Candy: 0.07,
+  Drink: 1.99,
 };
-var items=_.keys(prices);
-var width = 20;
-var item_props = [
-	{
-		name: 'item',
-		validator: /^[0-9]+$/,
-		warning: 'Input must be numeric'
-	}
+const items = _.keys(prices);
+const width = 20;
+const itemProps = [
+  {
+    name: 'item',
+    validator: /^[0-9]+$/,
+    warning: 'Input must be numeric',
+  },
 ];
-var amount_props = [
-	{
-		name: 'amount',
-		validator: /^[0-9\.]+$/,
-		warning: 'Input must be a float'
-	}
+const amountProps = [
+  {
+    name: 'amount',
+    validator: /^[0-9.]+$/,
+    warning: 'Input must be a float',
+  },
 ];
 
-for (let i=0; i<items.length; i++) {
-	console.log((i+1) + ". " + items[i] + (" ".repeat(width-items[i].length)) + prices[items[i]]);
+for (let i = 0; i < items.length; i++) {
+  console.log(`${i + 1}. ${items[i]}${(' '.repeat(width - items[i].length))}${prices[items[i]]}`);
 }
 
-prompt.start();
-prompt.get(item_props, (err, res)=>{
-	if (err)
-		return onError(err);
-	if (res.item > items.length || res.item < 1)
-		return onError(new Error('Chosen item does not exist.'));
-
-	var price = prices[items[res.item-1]];
-	prompt.get(amount_props, (err, res)=>{
-		if (err)
-			return onError(err);
-
-		console.log('Your change: ' + change.calculate(price, res.amount));
-	});
-});
-
-function onError(err) {
-	console.log(err.message);
-	return 1;
+const onError = function onError(err) {
+  console.log(err.message);
+  process.exit();
 };
+
+let price = -1;
+Promise.promisifyAll(prompt);
+prompt.start();
+prompt.getAsync(itemProps).then((response) => {
+  if (response.item > items.length || response.item < 1) {
+    onError(new Error('Chosen item does not exist.'));
+  }
+  price = prices[items[response.item - 1]];
+  return prompt.getAsync(amountProps);
+}).then((response) => {
+  const change = calculateChange(price, parseFloat(response.amount));
+  console.log(`Your change: ${JSON.stringify(change)}`);
+});
