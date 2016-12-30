7f77f306bbdec975ee98f43a592cc5c2f8ac20e3
Alexander Filatov <alefi87@gmail.com>
2016-12-30 14:22:38 +0100

    Refactored Palindrome according to AirBnB ESLint rules

--- strings/palindrome/index.js
+++ strings/palindrome/index.js
@@ -1,12 +1,12 @@
 'use strict';
 
-exports.isPalindrome = (text)=>{
-	if (!text)
-		throw new TypeError('empty input');
+exports.isPalindrome = (text) => {
+  if (!text) {
+    throw new TypeError('empty input');
+  }
+  if (text.match(/[^a-z]/i)) {
+    throw new TypeError('non-alphabetic input');
+  }
 
-	if (text.match(/[^a-z]/i))
-		throw new TypeError('non-alphabetic input');
-
-	let result = text === text.split('').reverse().join('');
-	return result;
+  return text === text.split('').reverse().join('');
 };
--- strings/test/palindrome/palindrome-test.js
+++ strings/test/palindrome/palindrome-test.js
@@ -1,20 +1,22 @@
 'use strict';
 
-var expect = require('chai').expect;
-var isPalindrome = require('../../palindrome').isPalindrome;
+const expect = require('chai').expect;
+const isPalindrome = require('../../palindrome').isPalindrome;
 
-describe('Palindrome', ()=>{
-	it('throws TypeError when input is empty', ()=>{
-		expect(isPalindrome.bind(isPalindrome, '')).to.throw(/empty input/);
-	});
-	it('throws TypeError when input contains anything but alphabetic characters', ()=>{
-		expect(isPalindrome.bind(isPalindrome, 'r4c3c4r')).to.throw(/non-alphabetic/);
-		expect(isPalindrome.bind(isPalindrome, 'racercar is awesome')).to.throw(/non-alphabetic/);
-	});
-	it('identifies a palindrome', ()=>{
-		expect(isPalindrome('racecar')).to.be.true;
-	});
-	it('identifies non-palindromes', ()=>{
-		expect(isPalindrome('racer')).to.be.false;
-	});
+describe('Palindrome', () => {
+  it('throws TypeError when input is empty', () => {
+    expect(isPalindrome.bind(isPalindrome, '')).to.throw(/empty input/);
+  });
+  it('throws TypeError when input contains anything but alphabetic characters', () => {
+    expect(isPalindrome.bind(isPalindrome, 'r4c3c4r')).to.throw(/non-alphabetic/);
+    expect(isPalindrome.bind(isPalindrome, 'racercar is awesome')).to.throw(/non-alphabetic/);
+  });
+  it('identifies a palindrome', () => {
+    // eslint-disable-next-line no-unused-expressions
+    expect(isPalindrome('racecar')).to.be.true;
+  });
+  it('identifies non-palindromes', () => {
+    // eslint-disable-next-line no-unused-expressions
+    expect(isPalindrome('racer')).to.be.false;
+  });
 });
